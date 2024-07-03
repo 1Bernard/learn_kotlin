7bc0f8eb140db98673cb282752bc2fb75a1b0ec8
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-07-03 11:13:30 +0000

    Constants and Variables

---
+++ src/hello.kt
@@ -0,0 +1,17 @@
+fun main(args:Array<String>) {
+    // Constants
+    val number: Int = 10  // Int: Stores integers
+    val pie: Double = 3.24567775 // Double: Stores Decimals
+    val k: Float = 6.53F // Float stores minimal length of numbers after the decimal point
+    val j: String = "Hello"
+
+    //Constants are variables which it's value doesn't change thus it can't be reassigned another value.
+
+    // Variable
+    var variableNumber: Int = 99
+    variableNumber = 55
+    var personAge: Int = 22
+    personAge = 3
+
+    println("Hello Master Coding Family")
+}
