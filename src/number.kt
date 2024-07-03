f61bf678db4f361e20ddbf4ce0eb2902b1a49608
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-07-03 12:04:56 +0000

    Numbers, Strings, Boolean

---
+++ src/number.kt
@@ -0,0 +1,28 @@
+fun main() {
+    //Numbers
+    val s: Short = 12
+    val b: Byte = 120
+    val i: Int = -100000000
+    val l: Long = 7252987868689
+    val f: Float = 7234.234f
+    val d: Double = 834957.456456
+
+    // Character
+    val letter: Char = 'k'
+
+    // Boolean: Have either true or false
+    val flag: Boolean = true
+
+    //Strings
+    val stringA: String = "Hello Everyone" //String Literals
+
+    // Concatenation
+    val stringB: String = "Hi "
+    val stringC: String = "Friends"
+    val stringD: String = stringB + stringC
+
+    //String template
+    val stringN: String = "Jack"
+    val stringK: String = "Hello my name is $stringN"
+    println(stringK)
+}
