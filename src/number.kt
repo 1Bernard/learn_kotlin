7b7be5227734b416803e2693bb7569f70301b9c8
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-07-04 18:28:20 +0000

    Numbers, Strings, Boolean

--- src/number.kt
+++ src/number.kt
@@ -24,5 +24,24 @@ fun main() {
     //String template
     val stringN: String = "Jack"
     val stringK: String = "Hello my name is $stringN"
-    println(stringK)
+
+    //Pair and Triples
+    // (x, y)   (x,y,z)
+
+    val coordinates: Pair<Int, Int> = Pair(2, 3)
+    val coordinates2 = Pair(2, 3)    //Type interface is normally used in assigning a value
+    // to a variable where the Type of the value is not Explicitly stated
+    val coordinates3 = 2 to 3       // using "to" operator
+
+    //println("The Coordinates: ${coordinates.first}")
+    //println("The Coordinates: ${coordinates.second}")
+
+    val coordinates3D: Triple<Int, Int, Int> = Triple(1, 2, 3)
+    val coordinates3D_2 = Triple(3,4, 5)
+    println("The 3D coordinates: $coordinates3D")
+
+    val x = coordinates3D.first
+    val y = coordinates3D.second
+    val z = coordinates3D.third
+
 }
